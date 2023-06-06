22a30598c8cb40f30e9047d6ee227ea22bbcfd74
irakli nozadze <iraklinozadze2509@gmail.com>
2023-06-06 10:12:05 +0400

    Initial Commit

---
+++ Checklists/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by Irakli Nozadze on 06.06.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
