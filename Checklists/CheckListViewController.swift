9bffd5f77068f7bd68f753c7a2ed09f0f20772d0
irakli nozadze <iraklinozadze2509@gmail.com>
2023-06-06 12:01:49 +0400

    feat: toggle checkmark

--- Checklists/CheckListViewController.swift
+++ Checklists/CheckListViewController.swift
@@ -14,12 +14,12 @@ class CheckListViewController: UITableViewController {
     // Do any additional setup after loading the view.
   }
   
-  //    MARK: TableVIew Data Source
+  //  MARK: TableVIew Data Source
   override func tableView(
     _ tableView: UITableView,
     numberOfRowsInSection section: Int
   ) -> Int {
-    return 1
+    return 100
   }
   
   override func tableView(
@@ -29,7 +29,37 @@ class CheckListViewController: UITableViewController {
     let cell = tableView.dequeueReusableCell(
       withIdentifier: "ChecklistItem",
       for: indexPath)
+    
+    let label = cell.viewWithTag(1000) as! UILabel
+    if indexPath.row % 5 == 0 {
+      label.text = "Walk the dog"
+    } else if indexPath.row % 5 == 1 {
+      label.text = "Brush my teeth"
+    } else if indexPath.row % 5 == 2 {
+      label.text = "Learn iOS development"
+    } else if indexPath.row % 5 == 3 {
+      label.text = "Soccer practice"
+    } else if indexPath.row % 5 == 4 {
+      label.text = "Eat ice cream"
+    }
+    
     return cell
+  }
+  
+  //  MARK: TableView Delegate
+  override func tableView(
+    _ tableView: UITableView,
+    didSelectRowAt indexPath: IndexPath
+  ) {
+    if let cell = tableView.cellForRow(at: indexPath) {
+      if cell.accessoryType == .none {
+        cell.accessoryType = .checkmark
+      } else {
+        cell.accessoryType = .none
+      }
+    }
+    
+    tableView.deselectRow(at: indexPath, animated: true)
   }
 }
 
