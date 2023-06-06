751e3f7704e3bc32f0d95c698a404bddbf7fa7ea
irakli nozadze <iraklinozadze2509@gmail.com>
2023-06-06 14:31:10 +0400

    feat: add navbar and navbar add item

--- Checklists/CheckListViewController.swift
+++ Checklists/CheckListViewController.swift
@@ -14,23 +14,9 @@ class CheckListViewController: UITableViewController {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
     
-    let item1 = ChecklistItem()
-    item1.text = "Walk the dog"
-    items.append(item1)
-    let item2 = ChecklistItem()
-    item2.text = "Brush my teeth"
-    item2.checked = true
-    items.append(item2)
-    let item3 = ChecklistItem()
-    item3.text = "Learn iOS development"
-    item3.checked = true
-    items.append(item3)
-    let item4 = ChecklistItem()
-    item4.text = "Soccer practice"
-    items.append(item4)
-    let item5 = ChecklistItem()
-    item5.text = "Eat ice cream"
-    items.append(item5)
+    navigationController?.navigationBar.prefersLargeTitles = true
+    
+    simulateItemsInChecklist()
   }
   
   //  MARK: TableVIew Data Source
@@ -86,5 +72,40 @@ class CheckListViewController: UITableViewController {
   ){
     let label = cell.viewWithTag(1000) as! UILabel
     label.text = item.text
+  }
+  
+  //  MARK: Actions
+  @IBAction func addItem() {
+    let newRowIndex = items.count
+    
+    let item = ChecklistItem()
+    item.text = "I am new item"
+    items.append(item)
+    
+    let indexPath = IndexPath(row: newRowIndex, section: 0)
+    let indexPaths = [indexPath]
+    tableView.insertRows(at: indexPaths, with: .automatic)
+  }
+  
+  
+  //  MARK: Private
+  private func simulateItemsInChecklist() {
+    let item1 = ChecklistItem()
+    item1.text = "Walk the dog"
+    items.append(item1)
+    let item2 = ChecklistItem()
+    item2.text = "Brush my teeth"
+    item2.checked = true
+    items.append(item2)
+    let item3 = ChecklistItem()
+    item3.text = "Learn iOS development"
+    item3.checked = true
+    items.append(item3)
+    let item4 = ChecklistItem()
+    item4.text = "Soccer practice"
+    items.append(item4)
+    let item5 = ChecklistItem()
+    item5.text = "Eat ice cream"
+    items.append(item5)
   }
 }
